106716404cca0ddbcaf09df7eb1f2e44e2f12a11
florimondmanca <florimond.manca@protonmail.com>
2023-01-26 22:19:08 +0100

    Add OP_CONSTANT

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,13 @@
 enum OpCode {
+    Constant,
     Return,
 }
 
 impl From<OpCode> for u8 {
     fn from(value: OpCode) -> Self {
         match value {
-            OpCode::Return => 0,
+            OpCode::Constant => 0,
+            OpCode::Return => 1,
         }
     }
 }
@@ -15,39 +17,57 @@ impl TryFrom<u8> for OpCode {
 
     fn try_from(value: u8) -> Result<Self, Self::Error> {
         match value {
-            0 => Ok(OpCode::Return),
+            0 => Ok(OpCode::Constant),
+            1 => Ok(OpCode::Return),
             _ => Err(format!("Unknown opcode {value}")),
         }
     }
 }
 
+type Value = f64;
+
 struct Chunk {
     code: Vec<u8>,
+    constants: Vec<Value>,
 }
 
 impl Chunk {
     pub fn new() -> Self {
-        Self { code: Vec::new() }
+        Self {
+            code: Vec::new(),
+            constants: Vec::new(),
+        }
     }
 
     pub fn write(&mut self, byte: u8) {
         self.code.push(byte);
     }
 
+    pub fn add_constant(&mut self, value: Value) -> u8 {
+        self.constants.push(value);
+        self.constants.len() as u8 - 1
+    }
+
     pub fn disassemble(&self, name: &str) -> String {
         let mut f = format!("== {name} ==\n");
+
         let mut offset = 0;
+
         while offset < self.code.len() {
             offset = self.disassemble_instruction(&mut f, offset);
         }
+
         f
     }
 
     fn disassemble_instruction(&self, f: &mut String, offset: usize) -> usize {
         f.push_str(&format!("{offset:04} "));
+
         let instruction = self.code[offset];
+
         match OpCode::try_from(instruction) {
             Ok(opcode) => match opcode {
+                OpCode::Constant => self.constant_instruction(f, "OP_CONSTANT", offset),
                 OpCode::Return => simple_instruction(f, "OP_RETURN", offset),
             },
             Err(_) => {
@@ -55,6 +75,17 @@ impl Chunk {
                 offset + 1
             }
         }
+    }
+
+    fn constant_instruction(&self, f: &mut String, name: &str, offset: usize) -> usize {
+        let constant_index = self.code[offset + 1] as usize;
+
+        f.push_str(&format!(
+            "{name:-16} {constant_index:4} '{constant}'\n",
+            constant = self.constants.get(constant_index).unwrap()
+        ));
+
+        offset + 2
     }
 }
 
@@ -65,6 +96,9 @@ fn simple_instruction(f: &mut String, name: &str, offset: usize) -> usize {
 
 fn main() {
     let mut chunk = Chunk::new();
+    let constant_index = chunk.add_constant(1.2);
+    chunk.write(OpCode::Constant.into());
+    chunk.write(constant_index);
     chunk.write(OpCode::Return.into());
     println!("{}", chunk.disassemble("test chunk"));
 }
